63f243bef7d6b32f7c85c53438d0ac6490b7e6f2
Brian Coca <brian.coca+git@gmail.com>
2020-07-09 14:49:46 -0400

    avoid making delegated vars available w/o host

--- lib/ansible/vars/manager.py
+++ lib/ansible/vars/manager.py
@@ -426,10 +426,10 @@ class VariableManager:
         if task:
             all_vars['environment'] = task.environment
 
-        # if we have a task and we're delegating to another host, figure out the
-        # variables for that host now so we don't have to rely on hostvars later
-        if task and task.delegate_to is not None and include_delegate_to:
-            all_vars['ansible_delegated_vars'], all_vars['_ansible_loop_cache'] = self._get_delegated_vars(play, task, all_vars)
+            # if we have a task and we're delegating to another host, figure out the
+            # variables for that host now so we don't have to rely on hostvars later
+            if host and task.delegate_to is not None and include_delegate_to:
+                all_vars['ansible_delegated_vars'], all_vars['_ansible_loop_cache'] = self._get_delegated_vars(play, task, all_vars)
 
         # 'vars' magic var
         if task or play:
