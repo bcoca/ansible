d4ede3939bfad10dc49952b49a155e56a801a51e
Brian Coca <brian.coca+git@gmail.com>
2023-10-06 13:20:04 -0400

    hrmphf

---
+++ lib/ansible/modules/display.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2012 Dag Wieers <dag@wieers.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = r'''
+---
+module: debug
+short_description: Print statements during execution
+description:
+- This module prints statements during execution.
+version_added: '2.16'
+options:
+  msg:
+    description:
+    - The customized message that is printed. If omitted, prints a generic message.
+    type: str
+    required: yes
+  channel:
+    description:
+    - Allows you to use existing display channels in the Ansible core.
+      This will format and color and emit messages as Ansible itself would
+    type: str
+    choices:
+        display: normal text
+        error: show an error message
+        warning: emit a warning
+        deprecation: present a deprecation message
+        verbose: is the same as what ``-v`` uses
+        callback: normal task output
+    default: callback
+extends_documentation_fragment:
+- action_common_attributes
+- action_common_attributes.conn
+- action_common_attributes.flow
+
+attributes:
+    action:
+        support: full
+    async:
+        support: none
+    bypass_host_loop:
+        support: none
+    become:
+        support: none
+    check_mode:
+        support: full
+    diff_mode:
+        support: none
+    connection:
+        support: none
+    delegation:
+        details: Aside from C(register) and/or in combination with C(delegate_facts), it has little effect.
+        support:  partial
+    platform:
+        support: full
+        platforms: all
+seealso:
+- module: ansible.builtin.assert
+- module: ansible.builtin.fail
+author:
+- Dag Wieers (@dagwieers)
+- Michael DeHaan
+'''
+
+EXAMPLES = r'''
+- name: Print the gateway for each host when defined
+  ansible.builtin.debug:
+    msg: System {{ inventory_hostname }} has gateway {{ ansible_default_ipv4.gateway }}
+  when: ansible_default_ipv4.gateway is defined
+
+- name: Get uptime information
+  ansible.builtin.shell: /usr/bin/uptime
+  register: result
+
+- name: Print return information from the previous task
+  ansible.builtin.debug:
+    var: result
+    verbosity: 2
+
+- name: Display all variables/facts known for a host
+  ansible.builtin.debug:
+    var: hostvars[inventory_hostname]
+    verbosity: 4
+
+- name: Prints two lines of messages, but only if there is an environment value set
+  ansible.builtin.debug:
+    msg:
+    - "Provisioning based on YOUR_KEY which is: {{ lookup('ansible.builtin.env', 'YOUR_KEY') }}"
+    - "These servers were built using the password of '{{ password_used }}'. Please retain this for later use."
+'''
--- lib/ansible/plugins/action/display.py
+++ lib/ansible/plugins/action/display.py
@@ -14,35 +14,30 @@ class ActionModule(ActionBase):
     ''' Print statements during execution '''
 
     TRANSFERS_FILES = False
-    _VALID_ARGS = frozenset(('msg', 'channel', 'color'))
+    _VALID_ARGS = frozenset(('msg', 'channel', 'squelch'))
     _VALID_CHANNELS = frozenset(('deprecated', 'warning', 'display', 'verbose', 'error', 'callback'))
 
     def run(self, tmp=None, task_vars=None):
 
         try:
-            try:
-                msg = self._task.args['msg']
-            except KeyError:
-                raise AnsibleActionFail("The required field 'msg' is missing")
-
-            channel = self._task.args.get('channel', 'callback')
-
-            if channel not in self._VALID_CHANNELS:
-                raise AnsibleActionFail("Invalid channel '%s', valid values are: %s " % (channel, ', '.join(self._VALID_CHANNELS)))
-
-            result = super(ActionModule, self).run(tmp, task_vars)
-
-            if channel != 'callback':
-                call = getattr(display, channel)
-                call(to_text(msg, errors='surrogate_or_strict'))
-            else:
-                result['msg'] = msg
-
-            #result['_ansible_verbose_override'] = True
-            result['_ansible_always_verbose'] = True
-            result['_ansible_display_fields'] = ['mg']
-            result['failed'] = False
-        except TypeError:
+            msg = self._task.args['msg']
+        except KeyError:
             raise AnsibleActionFail("The required field 'msg' is missing")
+
+        channel = self._task.args.get('channel', 'callback')
+
+        if channel not in self._VALID_CHANNELS:
+            raise AnsibleActionFail("Invalid channel '%s', valid values are: %s " % (channel, ', '.join(self._VALID_CHANNELS)))
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+        result['failed'] = False
+
+        if channel == 'callback':
+            result['msg'] = msg
+            result['_ansible_verbose_override'] = True
+            result['_ansible_always_verbose'] = True
+        else:
+            call = getattr(display, channel)
+            call(to_text(msg, errors='surrogate_or_strict'))
 
         return result
